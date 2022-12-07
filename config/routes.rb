c536eb03ca78e63f1c397bb14e69febf859aaa79
Breandan <bwaddellflynn@gmail.com>
2022-12-06 21:08:20 -0600

    Cart now functions. Bug persists where form submission will delete session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+  get 'order_items/update'
+  get 'order_items/destroy'
+  get 'cart/show'
   resources :products
   get '/search', to: 'search#index'
   resources :admin_contents
